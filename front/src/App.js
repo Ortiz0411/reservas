b39c002fb13905718a32ae065171417484550a2f
Ortiz0411 <a.ortizmartinez0411@gmail.com>
2023-09-13 22:05:12 -0600

    getByName

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,123 +6,123 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
+const url = "http://localhost:4000/service/";
 
 
-const url="http://localhost:4000/service/";
 
-class App extends Component{
-  
-  state={
-    data:[],
-    modalInsertar: false, 
-    modalEliminar: false,
-    form:{
-      id:'',
-      name:'',
-      category:'',
-      description:'',
-      rackPrice:'',
-      netPrice:'',
-      tax:'',
-      tipoModal:''
-      
+function App() {
+  const [services, setServices] = useState([]);
+  const [search, setSearch ] = useState("");
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [form, setForm] = useState({
+    id: '',
+    name: '',
+    category: '',
+    description: '',
+    rackPrice: '',
+    netPrice: '',
+    tax: '',
+    tipoModal: ''
+  });
+
+  useEffect(() => {
+    get();
+  }, []);
+
+  const get = async () => {
+    try {
+      const response = await axios.get(url + "getServices");
+      setServices(response.data);
+    } catch (error) {
+      console.log(error.message);
     }
-     
-  }
-
-
-  get=()=>{
-    axios.get(url+ "getServices").then(response=>{
-      this.setState({data: response.data});
-    }).catch(error=>{
-      console.log(error.message);
-    })
-  }
-
-
-  peticionPost=async()=>{
-    
-   await axios.post(url + "addService",this.state.form).then(response=>{
-      this.modalInsertar();
-      this.get();
-    }).catch(error=>{
-      console.log(error.message);
-    })
   };
 
+  const peticionPost = async () => {
+    try {
+      await axios.post(url + "addService", form);
+      toggleModalInsertar(); 
+      get();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-  peticionPut = () => {
+  const peticionPut = () => {
     axios
-      .put(url + "updateService/" + this.state.form.id, this.state.form)
-      .then((response) => {
-        this.modalInsertar();
-        this.get();
+      .put(url + "updateService/" + form.id, form)
+      .then(() => {
+        toggleModalInsertar(); 
+        get();
       })
       .catch((error) => {
         console.log(error.message);
-      });
+      });
   };
 
+  const peticionDelete = () => {
+    axios.delete(url + "deleteService/" + form.id)
+      .then(() => {
+        setModalEliminar(false);
+        get();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-  peticionDelete=()=>{
-    axios.delete(url+"deleteService/"+this.state.form.id).then(response=>{
-     this.setState({modalEliminar: false});
-      this.get();
-    }).catch(error=>{
-      console.log(error.message);
-    })
-    console.log(this.state.form.id);
-  }
-
-
-  selectService = (service) => {
-    this.setState({
-      tipoModal: 'actualizar',
-      form: {
-        id: service.id,
-        name: service.name,
-        category: service.category,
-        description: service.description,
-        rackPrice: service.rackPrice,
-        netPrice: service.netPrice,
-        tax: service.tax,
-      },
+  const selectService = (service) => {
+    setForm({
+      id: service.id,
+      name: service.name,
+      category: service.category,
+      description: service.description,
+      rackPrice: service.rackPrice,
+      netPrice: service.netPrice,
+      tax: service.tax,
+      tipoModal: 'actualizar'
     });
   };
+  
+  const toggleModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
+  };
 
-
-  modalInsertar=()=>{
-    this.setState({modalInsertar: !this.state.modalInsertar});
+  let resultado = [];
+  if(!search){
+      resultado = services;
+  }else {
+    resultado = services.filter((dato)=>
+    dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+    
+    )
   }
 
+  const searcher = async (e) => {
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  };
 
-  componentDidMount(){
-    this.get();
-  }
- 
-
-  handleChange = async (e) => {
+  const handleChange = async (e) => {
     e.persist();
-    await this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value 
-      }
+    await setForm({
+      ...form,
+      [e.target.name]: e.target.value
     });
-    console.log(this.state.form);
-  }
+  };
 
+  
 
-  render(){
-    const {form}=this.state;
-    return(
-     <div className="app">
-      <br/>
-      <div className="center-content"> 
-        <button className="btn btn-success" onClick={()=>{this.setState({form: null, tipoModal: 'insertar'}); this.modalInsertar()}}>Agregar servicio</button>
+  return (
+    <div className="app">
+      <br />
+      <div className="center-content">
+      <button className="btn btn-success" onClick={() => { setForm(null); setForm({ tipoModal: 'insertar' }); setModalInsertar(true); }}>Agregar servicio</button>
       </div>
+      <input value={search} placeholder='Buscar...' onChange={searcher} type="text" name="value" id="value"/>
       <br /><br />
-        <table className="table">
+      <table className="table">
           <thead>
             <tr>
             <th>Id del servicio</th>
@@ -136,7 +136,7 @@ class App extends Component{
             </tr>
           </thead>
           <tbody>
-             {this.state.data.map(service=>{
+             {resultado.map(service=>{
               return(
                  <tr> 
               <td>{service.id}</td>
@@ -147,9 +147,9 @@ class App extends Component{
               <td>{new Intl.NumberFormat("en-EN").format(service.netPrice)}</td>
               <td>{new Intl.NumberFormat("en-EN").format(service.tax)}</td>
               <td>
-              <button className="btn btn-primary" onClick={()=>{this.selectService(service); this.modalInsertar()}}><FontAwesomeIcon icon={faEdit}/></button>
-                {"   "}
-                <button className="btn btn-danger" onClick={()=>{this.selectService(service); this.setState({modalEliminar: true})}}><FontAwesomeIcon icon={faTrashAlt}/></button>
+              <button className="btn btn-primary" onClick={() => { selectService(service); toggleModalInsertar(); }}> <FontAwesomeIcon icon={faEdit} /></button>
+              {"   "}
+              <button className="btn btn-danger" onClick={() => { selectService(service); setModalEliminar(true); }}><FontAwesomeIcon icon={faTrashAlt} /></button>
                 </td>
               </tr>
               )  
@@ -158,67 +158,63 @@ class App extends Component{
         </table>
 
 
-
-        <Modal isOpen={this.state.modalInsertar}>
-                <ModalHeader style={{display: 'block'}}>
-                  <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
-                </ModalHeader>
-                <ModalBody>
-                  <div className="form-group">
-                    <label htmlFor="name">Nombre del servicio</label>
-                    <input className="form-control" type="text" name="name" id="name" onChange={this.handleChange} value={form?form.name : ''}/>
+        <Modal isOpen={modalInsertar}>
+            <ModalHeader style={{ display: 'block' }}>
+              <span style={{ float: 'right' }} onClick={() => toggleModalInsertar()}>x</span>
+            </ModalHeader>
+            <ModalBody>
+              <div className="form-group">
+                <label htmlFor="name">Nombre del servicio</label>
+                    <input className="form-control" type="text" name="name" id="name" onChange={handleChange} value={form ? form.name : ''} />
                     <br />
                     <label htmlFor="category">Categoria</label>
-                    <input className="form-control" type="text" name="category" id="category" onChange={this.handleChange} value={form ?form.category : ''}/>
+                    <input className="form-control" type="text" name="category" id="category" onChange={handleChange} value={form ?form.category : ''}/>
                     <br />
                     <label htmlFor="description">Descripción</label>
-                    <input className="form-control" type="text" name="description" id="description" onChange={this.handleChange} value={form ? form.description : ''}/>
+                    <input className="form-control" type="text" name="description" id="description" onChange={handleChange} value={form ? form.description : ''}/>
                     <br />
                     <label htmlFor="rackPrice">Tarifa rack</label>
-                    <input className="form-control" type="text" name="rackPrice" id="rackPrice" onChange={this.handleChange} value={form ? form.rackPrice : ''}/>
+                    <input className="form-control" type="text" name="rackPrice" id="rackPrice" onChange={handleChange} value={form ? form.rackPrice : ''}/>
 
                     <br />
                     <label htmlFor="netPrice">Precio neto</label>
-                    <input className="form-control" type="text" name="netPrice" id="netPrice" onChange={this.handleChange} value={form ? form.netPrice : ''}/>
+                    <input className="form-control" type="text" name="netPrice" id="netPrice" onChange={handleChange} value={form ? form.netPrice : ''}/>
                     <br />
                     <label htmlFor="tax">Impuestos</label>
-                    <input className="form-control" type="text" name="tax" id="tax" onChange={this.handleChange} value={form ? form.tax : ''}/>
-                  </div>
-                </ModalBody>
+                    <input className="form-control" type="text" name="tax" id="tax" onChange={handleChange} value={form ? form.tax : ''}/>
+              </div>
+            </ModalBody>
 
-                <ModalFooter>
-                {this.state.tipoModal==='insertar'?
-                
-                    <button className="btn btn-success" onClick={()=>this.peticionPost()}>
-                    Insertar
-                  </button>: <button className="btn btn-primary" onClick={()=>this.peticionPut()}>
-                    Actualizar
-                  </button>
-            }
-                    <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
-                </ModalFooter>
-        </Modal>
+            <ModalFooter>
+              {form.tipoModal === 'insertar' ?
+                <button className="btn btn-success" onClick={() => peticionPost()}>
+                  Insertar
+                </button> :
+                <button className="btn btn-primary" onClick={() => peticionPut()}>
+                  Actualizar
+                </button>
+              }
+              <button className="btn btn-danger" onClick={() => setModalInsertar(false)}>Cancelar</button>
+            </ModalFooter>
+          </Modal>
 
-
-        <Modal isOpen={this.state.modalEliminar}>
-          <ModalBody>
-            Estás seguro que deseas eliminar el servicio {form && form.name}
-          </ModalBody>
-          <ModalFooter>
-            <button className="btn btn-danger" onClick={() => this.peticionDelete()}>
-            Sí
-            </button>
-            <button className="btn btn-secondary" onClick={() => this.setState({ modalEliminar: false })}>
-            No
-             </button>
+          <Modal isOpen={modalEliminar}>
+            <ModalBody>
+              Estás seguro que deseas eliminar el servicio {form && form.name}
+            </ModalBody>
+            <ModalFooter>
+              <button className="btn btn-danger" onClick={() => peticionDelete()}>
+                Sí
+              </button>
+              <button className="btn btn-secondary" onClick={() => setModalEliminar(false)}>
+                No
+              </button>
             </ModalFooter>
         </Modal>
 
 
-     </div>
-    );
-  }
- 
+    </div>
+  );
 }
 
 export default App;
